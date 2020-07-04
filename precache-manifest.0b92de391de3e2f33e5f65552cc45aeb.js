4e097c1cb5ce3b3419c31bda527b0a8565e1d89d
miadv <miadsoft@gmail.com>
2020-07-04 18:11:45 +0800

    Updates

---
+++ precache-manifest.0b92de391de3e2f33e5f65552cc45aeb.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e967093040d06a49af855edb1273e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a45a71a4e336c2c48df9",
+    "url": "/static/css/main.35c3d76c.chunk.css"
+  },
+  {
+    "revision": "c4979bdeeb6da41b9066",
+    "url": "/static/js/0.d022e493.chunk.js"
+  },
+  {
+    "revision": "1f146453665b261501ecdb4f2c25ccdc",
+    "url": "/static/js/0.d022e493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6036dc6f7e9ca6d758",
+    "url": "/static/js/3.87b089b6.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/3.87b089b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267251b6f1dfa11ec05d",
+    "url": "/static/js/4.bee6470e.chunk.js"
+  },
+  {
+    "revision": "954c5c132bb4f4137c85",
+    "url": "/static/js/5.8abe3e3d.chunk.js"
+  },
+  {
+    "revision": "ee0c3a9052d4f21e07e0",
+    "url": "/static/js/6.23daf913.chunk.js"
+  },
+  {
+    "revision": "a45a71a4e336c2c48df9",
+    "url": "/static/js/main.1fe68c6c.chunk.js"
+  },
+  {
+    "revision": "35d5f1be87c15096aa66",
+    "url": "/static/js/runtime-main.2b39cc60.js"
+  },
+  {
+    "revision": "9e0504ad60e3362d682b08aafbe86cd8",
+    "url": "/static/media/1.9e0504ad.jpg"
+  },
+  {
+    "revision": "6ac6ed4aae11d142c5cbaea51eb3efcb",
+    "url": "/static/media/10.6ac6ed4a.jpg"
+  },
+  {
+    "revision": "fe991daf881512dad84e6724d286d899",
+    "url": "/static/media/11.fe991daf.jpg"
+  },
+  {
+    "revision": "dda4d77a46a168d9c9dc90e4fa9ecf49",
+    "url": "/static/media/12.dda4d77a.jpg"
+  },
+  {
+    "revision": "54ec9d7598418e7f22e42d259e673866",
+    "url": "/static/media/2.54ec9d75.jpg"
+  },
+  {
+    "revision": "54e853a3c2b88db4c9d645cb252ac363",
+    "url": "/static/media/3.54e853a3.jpg"
+  },
+  {
+    "revision": "a535cb0681a4adcf6570dd2bb70d7bf0",
+    "url": "/static/media/4.a535cb06.jpg"
+  },
+  {
+    "revision": "75997acba41e20aa7b7173dec646b8b6",
+    "url": "/static/media/5.75997acb.jpg"
+  },
+  {
+    "revision": "127f4b97ef1fa1c137c8dabdb6ef4440",
+    "url": "/static/media/6.127f4b97.jpg"
+  },
+  {
+    "revision": "14460aa5d2dba9b555d0935c05bab21d",
+    "url": "/static/media/7.14460aa5.jpg"
+  },
+  {
+    "revision": "4429d52731e3a3054ced9e2deddf6cb0",
+    "url": "/static/media/8.4429d527.jpg"
+  },
+  {
+    "revision": "e4cc348997c7b8a5bd37291346d99738",
+    "url": "/static/media/9.e4cc3489.jpg"
+  },
+  {
+    "revision": "bae4316f6f360f7cbb06994f0bfb44e0",
+    "url": "/static/media/EdenMembership.bae4316f.png"
+  },
+  {
+    "revision": "b65a1981634bcf159630790d3159a2f4",
+    "url": "/static/media/EdenScent-Home.b65a1981.png"
+  },
+  {
+    "revision": "049858719e33b0b26260e00ef5c0688d",
+    "url": "/static/media/EdenScent-ProductPage.04985871.png"
+  },
+  {
+    "revision": "abcc63ab12d65de305fa2c6cba078d98",
+    "url": "/static/media/EdenScent-app.abcc63ab.jpg"
+  },
+  {
+    "revision": "bd9e9a91d10884525302805f74c54ca4",
+    "url": "/static/media/Ejamas-Home.bd9e9a91.png"
+  },
+  {
+    "revision": "83c6886608baa2c227713e468d816052",
+    "url": "/static/media/Ejamas-ProductPage.83c68866.png"
+  },
+  {
+    "revision": "525045b9c894f5ed9b2a93c6e86f86f6",
+    "url": "/static/media/Ejamas-Products.525045b9.png"
+  },
+  {
+    "revision": "5fdcc196203bd53d29982b3c6b6047e1",
+    "url": "/static/media/Gilasco-Home.5fdcc196.png"
+  },
+  {
+    "revision": "50a9237194eed9af22742c209d7559cd",
+    "url": "/static/media/Isabella - Black-thumbnail.50a92371.jpg"
+  },
+  {
+    "revision": "c8f0df31647ab84a7887b09c22da4a2d",
+    "url": "/static/media/Isabella - Black.c8f0df31.mp4"
+  },
+  {
+    "revision": "4f8843b461c3218de42627001268de56",
+    "url": "/static/media/Isabella - Khaki-thumbnail.4f8843b4.jpg"
+  },
+  {
+    "revision": "b831ff9bedb033e667051e52c007ae31",
+    "url": "/static/media/Isabella - Khaki.b831ff9b.mp4"
+  },
+  {
+    "revision": "817862d5e30f14da6c9a9eabb80aa6ca",
+    "url": "/static/media/Santino-thumbnail.817862d5.jpg"
+  },
+  {
+    "revision": "377c765fedccd5b4fdb49b096416360f",
+    "url": "/static/media/Santino.377c765f.mp4"
+  },
+  {
+    "revision": "72aec7abe651e84940035bf4e6754271",
+    "url": "/static/media/TamayGroup-Home.72aec7ab.png"
+  },
+  {
+    "revision": "a11e50a8030efbb71c6ea5c86c76d8e7",
+    "url": "/static/media/Tamayshoes-Home.a11e50a8.png"
+  },
+  {
+    "revision": "909cf1c6c3ff806bc5c20659ed1bcf3d",
+    "url": "/static/media/Tamayshoes-ProductPage.909cf1c6.png"
+  },
+  {
+    "revision": "a6d799d9a253f0f703b10cf9cbf3b507",
+    "url": "/static/media/Tamayshoes-Products.a6d799d9.png"
+  },
+  {
+    "revision": "ef96b0fd01d526cad7e40ddb5dd2a2c1",
+    "url": "/static/media/bg1.ef96b0fd.jpg"
+  },
+  {
+    "revision": "456b47bce336ad14b0da2b679a1ea9e7",
+    "url": "/static/media/bg2.456b47bc.jpg"
+  },
+  {
+    "revision": "bade0f805119c4e72979c6455fec5e1f",
+    "url": "/static/media/bg3.bade0f80.jpg"
+  },
+  {
+    "revision": "ae3c874d0f8ad41aee2a32dae5fab77b",
+    "url": "/static/media/bg4.ae3c874d.jpg"
+  },
+  {
+    "revision": "d67305229d9cbfe677b1498d5941d5e0",
+    "url": "/static/media/bg5.d6730522.jpg"
+  },
+  {
+    "revision": "b7d546f19cd9a4387d02143caaac08d7",
+    "url": "/static/media/miad.b7d546f1.jpg"
+  }
+]);
